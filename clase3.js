eb57b6167600aecac88cb84d556c6ee51d7a7711
HarryPlaysXP <matiascorrales2002@gmail.com>
2026-03-26 22:33:02 -0300

    ej3

--- clase3.js
+++ clase3.js
@@ -49,4 +49,24 @@ console.log(frutas);
 frutas.push("kiwi");
 console.log(frutas);
 console.log("b)");
+const numeros = [1, 2, 3, 4, 5];
+console.log(numeros);
+const numeros2 = [];
+for (let i = 0; i < numeros.length; i++) {
+    numeros2.push(numeros[i] * 2);
+}
+console.log(numeros2);
 console.log("c)");
+const colores = ["rojo", "verde", "azul"];
+console.log(colores);
+const colores2 = ["amarillo", "magenta"];
+console.log(colores2);
+const todosLosColores = colores.concat(colores2);
+console.log(todosLosColores);
+console.log("d)");
+const elementos6 = ["Tierra", "Agua", "Fuego", "Aire", "Vacio","Plasma"];
+console.log(elementos6);
+elementos6.shift();
+console.log(elementos6);
+elementos6.pop();
+console.log(elementos6);
