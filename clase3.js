e330288aab50b7cf18902b7596f853c08d8c43c2
HarryPlaysXP <matiascorrales2002@gmail.com>
2026-03-26 22:44:39 -0300

    ej4-a

--- clase3.js
+++ clase3.js
@@ -70,3 +70,13 @@ elementos6.shift();
 console.log(elementos6);
 elementos6.pop();
 console.log(elementos6);
+console.log ("-".repeat(100))
+console.log ("-Ejercicio 3 - Objetos y sus propiedades")
+const libro = {
+    titulo: "El Gran Gatsby",
+    autor:"F. Scott Fitzgerald",
+    anioPublicacion: "1925"
+};
+console.log(libro.autor);
+console.log(libro.titulo);
+console.log(libro.anioPublicacion);
