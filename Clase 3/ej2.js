e8cb6d3ad2f20572fbba8d7d489d08ced28f242d
HarryPlaysXP <matiascorrales2002@gmail.com>
2026-04-07 20:46:02 -0300

    Clase 3 ej1-ej2

--- Clase 3/ej2.js
+++ Clase 3/ej2.js
@@ -1,3 +1,5 @@
+console.log ("Guia de ejercicios ES6")
+console.log ("-".repeat(100))
 console.log ("-Ejercicio 2 - Arrays")
 console.log("a)");
 const frutas = ["manzana", "banana", "pera", "naranja"];
@@ -29,12 +31,3 @@ console.log(elementos6);
 elementos6.pop();
 console.log(elementos6);
 console.log ("-".repeat(100))
-console.log ("-Ejercicio 3 - Objetos y sus propiedades")
-const libro = {
-    titulo: "El Gran Gatsby",
-    autor:"F. Scott Fitzgerald",
-    anioPublicacion: "1925"
-};
-console.log(libro.autor);
-console.log(libro.titulo);
-console.log(libro.anioPublicacion);
