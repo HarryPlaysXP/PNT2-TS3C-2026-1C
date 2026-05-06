4165f98dc39e0e6e34e60960a7782658fdc2448d
HarryPlaysXP <matiascorrales2002@gmail.com>
2026-05-06 07:59:07 -0300

    practica parcial - Clase 3 ej4

--- PracticasExamenes/Parcial1/Clase 3/ej4.js
+++ PracticasExamenes/Parcial1/Clase 3/ej4.js
@@ -6,3 +6,14 @@ const edad = 30;
 const profesion = "Desarrollador";
 const presentacion = `Hola, mi nombre es ${nombre}, tengo ${edad} años y soy ${profesion}.`;
 console.log(presentacion);
+console.log("b)Crea una función que reciba un precio y una cantidad, y devuelva una frase usando cálculos. La frase es “El total es: [total]“");
+const calcularTotal = (precio, cantidad) => {
+    const total = precio * cantidad;
+    return `El total es: ${total}`;
+};
+console.log(calcularTotal(10, 5));
+console.log("c) Utiliza template literals para crear un mensaje de error que incluya el nombre del error y su descripción.");
+const errorName = "Error de conexión";
+const errorDescription = "No se pudo conectar al servidor.";
+const errorMessage = `Error: ${errorName} - ${errorDescription}`;
+console.log(errorMessage);
